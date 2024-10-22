cf8adbc9edb0f1a27b1b0f184a2b8018eb0338e2
Samuel Forrest <samueljforrest@outlook.com>
2024-10-22 12:05:15 +0100

    pressable components stable

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,10 +1,10 @@
-import { View, Text, Button, Alert } from 'react-native'
+import { View, Text, Button, Alert, Pressable } from 'react-native'
 import React from 'react'
 
 const ButtonComponent = () => {
   return (
     <View>
-      <Text>ButtonComponent</Text>
+      <Text>Here are some buttons:</Text>
       <Button 
         title="PRESS ME!" 
         color="red"
@@ -13,8 +13,28 @@ const ButtonComponent = () => {
       <Button 
         title="Tap Me!"
         color="crimson"
+        onPress={() => console.log("Button Pressed Hard!")}>
+      </Button>
+
+      <Button 
+        title="Tap Me!"
+        color="green"
         onPress={() => console.log("Button Pressed!")}>
       </Button>
+
+      <Pressable
+        onPress={() => console.log("Pressable Pressed!")}>
+        <Text style={{color: 'blue'}}>Press Me! (Pressable)</Text>
+      </Pressable>
+
+      <Pressable onPressIn={() => console.log("Pressable Pressed In!")}>
+        <Text style={{color: 'blue'}}>Press Me (In)!</Text>
+      </Pressable>
+
+      <Pressable onPressOut={() => console.log("Pressable Pressed Out!")}>
+        <Text style={{color: 'blue'}}>Press Me (Out)!</Text>
+      </Pressable>
+
     </View>
   );
 };
