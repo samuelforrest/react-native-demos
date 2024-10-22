d7983b35ded0469ba9fc7f636825dedaa2b3ee08
Samuel Forrest <samueljforrest@outlook.com>
2024-10-22 14:01:34 +0100

    dynamic content with numbers

--- src/components/DynamicContent.tsx
+++ src/components/DynamicContent.tsx
@@ -4,10 +4,14 @@ import React from 'react'
 const DynamicContent = () => {
 
   const username = "Samuel Forrest";
+  const mutliply = (a: number,b: number) => a * b;
 
   return (
     <View>
       <Text>Hello, {username}!</Text>
+      <Text>2 * 3 = {mutliply(2,3)}</Text>
+      <Text>{2+2}</Text>
+      <Text>{2*10}</Text>
     </View>
   );
 };
