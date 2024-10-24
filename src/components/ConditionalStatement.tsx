a19ff39e9ebcaa026312995f6d16bb1bcea79141
Samuel Forrest <samueljforrest@outlook.com>
2024-10-24 15:46:11 +0100

    Pasword validation stable

--- src/components/ConditionalStatement.tsx
+++ src/components/ConditionalStatement.tsx
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+const ValidPassword = () => <Text>
+Valid Password
+</Text>
+const InvalidPassword = () => <Text>
+Invalid Password
+</Text>
+
+const Password = ({isValid}) => {
+  if (isValid) {
+    return <ValidPassword />
+  }
+
+  return <InvalidPassword />
+};
+
 const ConditionalStatement = () => {
   return (
     <View>
-      <Text>ConditionalStatmeent</Text>
+      <Password isValid={true}/>
     </View>
   );
 };
